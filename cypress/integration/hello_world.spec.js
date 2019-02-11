f8d8fbecb6fb52d8b3207dea7d712e97be4b1fd7
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-02-11 16:45:15 +0000

    Adding basic structure

---
+++ cypress/integration/hello_world.spec.js
@@ -0,0 +1,11 @@
+context('Hello World', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('cy.hash() - get the current URL hash', () => {
+        // https://on.cypress.io/hash
+        // cy.hash().should('be.empty')
+        cy.contains('Home page')
+    })
+})
