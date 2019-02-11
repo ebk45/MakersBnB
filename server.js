d1ff80300ea42eb1baa1254b900a55f52d0681cb
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-02-11 15:59:00 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+//server.js
+const express = require('express'),
+    server = express();
+
+server.set('port', process.env.PORT || 3000);
+
+//Basic routes
+server.get('/', (request,response)=>{
+    response.send('Home page');
+});
+
+server.get('/about',(request,response)=>{
+    response.send('About page');
+});
+
+//Express error handling middleware
+server.use((request,response)=>{
+    response.type('text/plain');
+    response.status(505);
+    response.send('Error page');
+});
+
+//Binding to a port
+server.listen(3000, ()=>{
+    console.log('Express server started at port 3000');
+});
