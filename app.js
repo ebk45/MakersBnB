7cd2ab66335c6d449df52fecad966a24ac7afc44
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-02-12 16:36:50 +0000

    Merging views over to master

--- app.js
+++ app.js
@@ -12,11 +12,33 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./server/routes')(app);
+
 app.get('/', (request,response)=>{
-    response.send('Home page');
+    response.render('pages/signup');
 });
+
+app.get('/feed',(request,response)=>{
+    response.render('pages/feed');
+});
+
+app.get('/listing', (request, response)=> {
+    response.render('pages/listing');
+});
+
+app.get('/spaces',(request,response)=>{
+    response.render('pages/spaces');
+});
+
+
 app.get('*', (req, res) => res.status(200).send({
     message: 'Welcome to the beginning of nothingness.',
 }));
+
+//Express error handling middleware
+app.use((request,response)=>{
+    response.type('text/plain');
+    response.status(505);
+    response.render('pages/error');
+});
 
 module.exports = app;
