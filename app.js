b5033c17ae2f32d9668fa9e55e7938d57489cedb
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-02-12 12:35:50 +0000

    Working, before routing

---
+++ app.js
@@ -0,0 +1,51 @@
+//app.js
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+
+
+const app = express();
+
+app.use(logger('dev'))
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('*', (req, res) => res.status(200).send({
+    message: 'Welcome to the beginning of nothingness.',
+}));
+
+
+
+
+
+
+//
+// app.set('port', process.env.PORT || 3000);
+//
+//
+// //Express error handling middleware
+// app.use((request, response)=>{
+//     response.type('text/plain');
+//     response.status(505);
+//     response.send('Error page');
+// });
+//
+// //Binding to a port
+// app.listen(3000, ()=>{
+//     console.log('Express server started at port 3000');
+// });
+
+module.exports = app;
+
+
+// //Basic routes
+// app.get('/', (request, response)=>{
+//     response.send('Home page');
+// });
+//
+// app.get('/about',(request, response)=>{
+//     response.send('About page');
+// });
--- db/migrations/01_create_listings_table.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE listings(id SERIAL PRIMARY KEY, name varchar(100), description varchar(4096), price DECIMAL(8,2) NOT NULL);
