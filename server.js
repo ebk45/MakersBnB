52167673dd7a022e915a2cedef967fad5cf7297e
ellie turnock <ebturnock@gmail.com>
2019-02-12 10:38:13 +0000

    rendered ejs views in server

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 //server.js
-const express = require('express'),
-    server = express();
+const express = require('express');
+const server = express();
 
 server.set('port', process.env.PORT || 3000);
 
 //Basic routes
 server.get('/', (request,response)=>{
-    response.send('Home page');
+    res.render('pages/signup');
 });
 
-server.get('/about',(request,response)=>{
-    response.send('About page');
+server.get('/spaces',(request,response)=>{
+    res.render('pages/spaces');
 });
 
 //Express error handling middleware
 server.use((request,response)=>{
     response.type('text/plain');
     response.status(505);
-    response.send('Error page');
+    res.render('pages/error');
 });
 
 //Binding to a port
