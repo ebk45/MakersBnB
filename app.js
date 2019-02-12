0683d24beebacaf850c67972141e855eefa7a911
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-02-12 15:55:13 +0000

    Tweak Cypress tests and update travis

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 
-
 const app = express();
 
 app.use(logger('dev'))
@@ -13,39 +12,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./server/routes')(app);
+app.get('/', (request,response)=>{
+    response.send('Home page');
+});
 app.get('*', (req, res) => res.status(200).send({
     message: 'Welcome to the beginning of nothingness.',
 }));
 
-
-
-
-
-
-//
-// app.set('port', process.env.PORT || 3000);
-//
-//
-// //Express error handling middleware
-// app.use((request, response)=>{
-//     response.type('text/plain');
-//     response.status(505);
-//     response.send('Error page');
-// });
-//
-// //Binding to a port
-// app.listen(3000, ()=>{
-//     console.log('Express server started at port 3000');
-// });
-
 module.exports = app;
-
-
-// //Basic routes
-// app.get('/', (request, response)=>{
-//     response.send('Home page');
-// });
-//
-// app.get('/about',(request, response)=>{
-//     response.send('About page');
-// });
