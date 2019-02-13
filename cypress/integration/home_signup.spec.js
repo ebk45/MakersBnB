1446f09ef2e5fca4461f80f0a233ee43f6aded96
James <james.rudrum@hyperisland.se>
2019-02-13 12:11:18 +0000

    Added test that checks page has correct form fields titles and text

--- cypress/integration/home_signup.spec.js
+++ cypress/integration/home_signup.spec.js
@@ -9,9 +9,15 @@ context('Home Page', function() {
     cy.contains('Sign Up')
   })
 
+  it('checks page has correct form fields titles and text', function() {
+    cy.contains('Name')
+    cy.contains('Email Address')
+    cy.contains('We\'ll never share your email with anyone else.')
+    cy.contains('Password')
+    cy.contains('What will use MakersBnB for?')
+  })
+
   it('fills fields in form and submits', function() {
-    // https://on.cypress.io/hash
-    // cy.hash().should('be.empty')
     cy.get('#inputName').type('King James')
     cy.get('#inputEmail').type('james@gmail.com')
     cy.get('#inputPassword').type('password123')
