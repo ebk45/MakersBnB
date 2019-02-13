24a9599180fe8c0ffc20a0a16252043e25dd3321
James <james.rudrum@hyperisland.se>
2019-02-13 10:50:05 +0000

    Updated gitignore and package.json and eslint fix app.js

--- app.js
+++ app.js
@@ -1,47 +1,45 @@
-//app.js
+// app.js
 const express = require('express')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 
-const app = express();
+const app = express()
 
 app.use(logger('dev'))
 
-app.set('view engine', 'ejs');
-
+app.set('view engine', 'ejs')
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
-require('./server/routes')(app);
+require('./server/routes')(app)
 
-app.get('/', (request,response)=>{
-    response.render('pages/signup');
-});
+app.get('/', (request, response) => {
+  response.render('pages/signup')
+})
 
-app.get('/feed',(request,response)=>{
-    response.render('pages/feed');
-});
+app.get('/feed', (request, response) => {
+  response.render('pages/feed')
+})
 
-app.get('/listing', (request, response)=> {
-    response.render('pages/listing');
-});
+app.get('/listing', (request, response) => {
+  response.render('pages/listing')
+})
 
-app.get('/spaces',(request,response)=>{
-    response.render('pages/spaces');
-});
-
+app.get('/spaces', (request, response) => {
+  response.render('pages/spaces')
+})
 
 app.get('*', (req, res) => res.status(200).send({
-    message: 'Welcome to the beginning of nothingness.',
-}));
+  message: 'Welcome to the beginning of nothingness.'
+}))
 
-//Express error handling middleware
-app.use((request,response)=>{
-    response.type('text/plain');
-    response.status(505);
-    response.render('pages/error');
-});
+// Express error handling middleware
+app.use((request, response) => {
+  response.type('text/plain')
+  response.status(505)
+  response.render('pages/error')
+})
 
-module.exports = app;
+module.exports = app
