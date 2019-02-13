fcd649c05942028eaf50dcad7abef414ff9a2db9
ellie turnock <ebturnock@gmail.com>
2019-02-13 16:32:49 +0000

    minor changes

--- server/models/listing.js
+++ server/models/listing.js
@@ -1,12 +1,14 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Listing = sequelize.define('Listing', {
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    price: DataTypes.INTEGER
-  }, {})
-  Listing.associate = function (models) {
-    // associations can be defined here
+  const Listing = sequelize.define(
+    'Listing', {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      price: DataTypes.INTEGER
+    }, {})
+  Listing.associate = ({ User }) => {
+    //Listing.belongsTo(User)
+    //can create more associations here
   }
   return Listing
 }
